60bc4b9f688883cc4467e2a5651ad52fa76ef0a9
Andreas Gohr <andi@splitbrain.org>
2011-06-10 09:29:17 +0200

    initial checkin

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the grouphome plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+$conf['grouppages']    = 'group:%s:start';
---
+++ lang/en/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * English language file for grouphome plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+
+$lang['grouppages'] = 'Where to find the group homepages. Use a <code>%s</code> where the group name should be inserted.';
+
+
+
+//Setup VIM: ex: et ts=4 :
