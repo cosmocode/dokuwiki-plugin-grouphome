b90b763b41dd6997f07d16fbc41011bd3fcc1d39
Myeongjin <aranet100@gmail.com>
2013-12-18 14:05:47 +0900

    Add Korean translation

---
+++ lang/ko/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Korean language file for grouphome plugin
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+
+$lang['grouppages'] = '그룹 홈페이지를 찾을 곳. 그룹 이름을 넣어야 할 곳에 <code>%s</code>(을)를 사용하세요.';
+
+
+
+//Setup VIM: ex: et ts=4 :
